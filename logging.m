cf39e759b8979bff3cdba000028ff24b2144f8c2
Kyukazari <89152474+Kyukazari@users.noreply.github.com>
2021-08-18 23:35:29 +0700

    Create logging.m
    Tempat menyimpan kata atau kalimat yang bot tidak mengerti

---
+++ logging.m
@@ -0,0 +1,13 @@
+\
+\ Copyright © 2021 Asta Team all rights reserved.
+\
+\ @author: Asta Team
+\ @date: 13 Aug 2021
+\
+\ A brief description of this program.
+\
+
+Q: $_
+A: Maaf, aku tidak mengerti maksud kamu.
+L: ${ last-question }
+--

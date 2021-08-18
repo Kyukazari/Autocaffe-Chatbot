146be846ef6d7d16a5504ad23f07b74049f84596
Kyukazari <89152474+Kyukazari@users.noreply.github.com>
2021-08-18 23:34:37 +0700

    Create kuis.m

---
+++ kuis.m
@@ -0,0 +1,74 @@
+\
+\ Copyright © 2021 Asta Team all rights reserved.
+\
+\ @author: Asta Team
+\ @date: 12 Aug 2021
+\
+\ A brief description of this program.
+\
+
+
+Q: Kuis
+A: https://tanya-tanya.com/wp-content/uploads/2019/09/sta65.png <br><br><br><br><br><br><br><br><br><p= align="justify">Diketahui data pada tabel diatas. Jika kuartil atasnya adalah 49,1. Tentukan nilai x</p> <br>${ "5" button2 } <p>${ "7" button2 } <p>${ "9" button2}</p>
+--
+
+Q: 5
+A: Jawaban kamu benar! &#128079 Lanjut soal no 2 ya ${ "soal2" button3 } 
+--
+
+Q: 7|9
+A: Jawaban kamu salah &#128532, jawaban yang benar adalah 5 ya sobat! Lanjut soal no 2 ya ${ "soal2" button3 }
+--
+
+Q: soal2
+A: <p align="justify">Dari 3 bilangan yang terkecil adalah 19 dan yang terbesar 75. Rata-rata hitung ketiga bilangan tersebut tidak mungkin sama dengan...</p> <br>${ "49" button2 } <p>${ "52" button2 } <p>${ "59" button2}</p>
+--
+
+Q: 59
+A: Jawaban kamu benar! &#128079 Lanjut soal no 3 ya ${ "soal3" button3 }
+--
+
+Q: 49|52
+A: Jawaban kamu salah &#128532, jawaban yang benar adalah 59 ya sobat! Lanjut soal no 3 ya ${ "soal3" button3 }
+--
+
+Q: soal3
+A: <p align="justify">Rata-rata nilai ulangan di suatu kelas adalah 7. Jika digabungkan dengan nilai 5 orang siswa lain yang rata-ratanya 8 maka nilai rata-ratanya menjadi 7,2. Berapa jumlah siswa pada kelompok pertama?</p> <br>${ "20" button2 } <p>${ "24" button2 } <p>${ "25" button2 }</p>
+--
+
+Q: 20
+A: Jawaban kamu benar! &#128079 Lanjut soal no 4 ya ${ "soal4" button3 }
+--
+
+Q: 24|25
+A: Jawaban kamu salah &#128532, jawaban yang benar adalah 20 ya sobat! Lanjut soal no 4 ya ${ "soal4" button3 }
+--
+
+Q: soal4
+A: <p align="justify">Ragam dari 20, 16, 14, 17, 13 adalah.... <br>${ "10" button2 } <p>${ "8" button2 } <p>${ "6" button2 }</p>
+--
+
+Q: 6
+A: Jawaban kamu benar! Lanjut soal no 5 ya ${ "soal5" button3 }
+--
+
+Q: 10|8
+A: Jawaban kamu salah &#128532, jawaban yang benar adalah 6 ya sobat! Lanjut soal no 5 ya ${ "soal5" button3 }
+--
+
+Q: soal5
+A: <p align="justify">Hasil nilai ulangan Matematika di suatu kelas disajikan dalam tabel frekuensi berikut. Jika rata-rata nilai ulangan itu adalah 83,5 maka nilai p adalah...</p> <br> https://1.bp.blogspot.com/-rgDF9xRxOW0/XWy-xkaSLFI/AAAAAAAAEd8/CASdTeKvtPgwf9FFa1C6Km9TIUmeAJzoQCLcBGAs/w400-h83/soal%2Bst1.jpg <p>${ "1" button2 } <p>${ "3" button2 } <p>${ "4" button2}</p>
+--
+
+Q: 3
+A: <p align="justify">Jawaban kamu benar! &#128079 Selamat sudah menyelesaikan kuis ini. Ayo itung berapa soal yang bisa kamu jawab benar. Jika kamu ingin mengerjakan soal lainnya bisa cek link berikut ${ "klik disini" "https://d1ahk.blogspot.com/2019/09/soal-statistika-kelas-12.html?m=1" link } ${ "&#128309 Menu &#128309" Button1 }
+--
+
+Q: 1|4
+A: Jawaban kamu salah &#128532, jawaban yang benar adalah 3 ya sobat! Selamat sudah menyelesaikan kuis ini. Ayo itung berapa soal yang bisa kamu jawab benar. Jika kamu ingin mengerjakan soal lainnya bisa cek link berikut ${ "klik disini" "https://d1ahk.blogspot.com/2019/09/soal-statistika-kelas-12.html?m=1" link } ${ "&#10024 Menu &#10024" Button1 }
+--
+
+
+
+
+
